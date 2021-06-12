2848f0e649f9d07d74303255e17732f027001715
eancedeg <eancedeg@gmail.com>
2021-06-12 03:43:01 -0400

    * requirements.txt modified

--- calculate/blast.py
+++ calculate/blast.py
@@ -25,13 +25,6 @@ class BlastCalculation(QtCore.QThread):
             seq.id = '_'.join(seq.description.split())
             seqs[seqindex] = seq
         return seqs
-        #SeqIO.write(seqs, 'temp.fasta', 'fasta')
-
-    # def clean_duplicates(self, raw_data):
-    #     raw_data['check_string'] = raw_data.apply(lambda row: ''.join(sorted([row['seq1'], row['seq2']])), axis=1)
-    #     no_dups = raw_data.drop_duplicates('check_string')
-    #     no_dups.index = pd.RangeIndex(start=1, stop=len(no_dups.index) + 1)
-    #     return no_dups
 
     def run(self):
         seqs = list(SeqIO.parse(self.fasta, 'fasta'))
@@ -40,42 +33,43 @@ class BlastCalculation(QtCore.QThread):
 
         if num_cases > unique_cases:
             seqs = self.modify_id(seqs)
+            SeqIO.write(seqs, 'temp.fasta', 'fasta')
             self.fasta = 'temp.fasta'
-            actual_cases = len(set([seq.id for seq in seqs]))
+            #actual_cases = len(set([seq.id for seq in seqs]))
 
-            if num_cases == actual_cases:
-                # Make Database
-                clinedb = NcbimakeblastdbCommandline(cmd='makeblastdb', dbtype='prot', input_file=self.fasta,
-                                                     input_type='fasta', out=self.fasta)
-                clinedb()
+        #     if num_cases == actual_cases:
+        # Make Database
+        clinedb = NcbimakeblastdbCommandline(cmd='makeblastdb', dbtype='prot', input_file=self.fasta,
+                                             input_type='fasta', out=self.fasta, parse_seqids=True)
+        clinedb()
 
-                # Calculation
-                clinec = NcbiblastpCommandline(cmd='blastp', query=self.fasta, db=self.fasta, evalue=0.1,
-                                               outfmt='6 qseqid sseqid pident evalue bitscore score',
-                                               max_target_seqs=num_cases, max_hsps=1, num_threads=self.cpucount,
-                                               out='all_vs_all.tsv')
-                clinec()
+        # Calculation
+        clinec = NcbiblastpCommandline(cmd='blastp', query=self.fasta, db=self.fasta, evalue=0.1,
+                                       outfmt='6 qseqid sseqid pident evalue bitscore score',
+                                       max_target_seqs=num_cases, max_hsps=1, num_threads=self.cpucount,
+                                       out='all_vs_all.tsv')
+        clinec()
 
-                # Data Processing
-                data = pd.read_csv('all_vs_all.tsv', delimiter='\t', names=['seq1', 'seq2', 'pident', 'evalue', 'bitscore',
-                                                                            'score'])
-                pivoted = data.pivot(index='seq1', columns='seq2', values=self.type[self.matrix_type])
+        # Data Processing
+        data = pd.read_csv('all_vs_all.tsv', delimiter='\t', names=['seq1', 'seq2', 'pident', 'evalue',
+                                                                    'bitscore', 'score'])
+        pivoted = data.pivot(index='seq1', columns='seq2', values=self.type[self.matrix_type])
 
-                for column in pivoted.columns:
-                    pos = pivoted.index.get_loc(column) + 1
-                    for index in pivoted.index[pos:]:
-                        if column != index:
-                            pivoted.loc[index, column] = pivoted.loc[column, index]
+        for column in pivoted.columns:
+            pos = pivoted.index.get_loc(column) + 1
+            for index in pivoted.index[pos:]:
+                if column != index:
+                    pivoted.loc[index, column] = pivoted.loc[column, index]
 
-                os.remove('all_vs_all.tsv')
-                os.remove('{}.phr'.format(self.fasta))
-                os.remove('{}.pin'.format(self.fasta))
-                os.remove('{}.psq'.format(self.fasta))
+        os.remove('all_vs_all.tsv')
+        os.remove('{}.phr'.format(self.fasta))
+        os.remove('{}.pin'.format(self.fasta))
+        os.remove('{}.psq'.format(self.fasta))
 
-                pivoted_round = pivoted.round(2)
+        pivoted_round = pivoted.round(2)
 
-                self.onfinished.emit(pivoted_round)
-            elif num_cases > actual_cases:
-                self.onincompletedata.emit('Duplicate sequences in "fasta"')
-            else:
-                self.onblastincomplete.emit("El blast no completó el total de los casos")
+        self.onfinished.emit(pivoted_round)
+            # elif num_cases > actual_cases:
+            #     self.onincompletedata.emit('Duplicate sequences in "fasta"')
+            # else:
+            #     self.onblastincomplete.emit("El blast no completó el total de los casos")
